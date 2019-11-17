d30a2b975f2c8fb609078822671ff3354ffbcd72
Rodrigo Conte <rodrigo.conte14@gmail.com>
2019-11-16 22:52:08 -0400

    Adding templates

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from taxes import ICMS, ISS
+from taxes import *
 
 class Tax_calculator(object):
     def performs_calculation(self, budget, tax):
@@ -7,10 +7,20 @@ class Tax_calculator(object):
         print( calculated_tax )
 
 if __name__ == '__main__':
-    from budget import Budget
+    from budget import Budget, Item
     
     calculator = Tax_calculator()
-    budget = Budget(500)
 
+    budget = Budget()
+    budget.add_item(Item('Item 1 - ', 50))
+    budget.add_item(Item('Item 1 - ', 200))
+    budget.add_item(Item('Item 1 - ', 250))
+
+    print('ISS and ICMS')
     calculator.performs_calculation(budget, ICMS())
     calculator.performs_calculation(budget, ISS())
+
+    
+    print('ICPP and IKCV')
+    calculator.performs_calculation(budget, ICPP())
+    calculator.performs_calculation(budget, IKCV())
--- taxes.py
+++ taxes.py
@@ -1,4 +1,29 @@
 # -*- coding: UTF-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+class Template_of_tax_conditional(object):
+    
+    __metaclass__ = ABCMeta
+
+    def calculate(self, budget):
+        if self.could_use_max_tax(budget):
+            return self.max_tax(budget)
+        else:
+            return self.min_tax(budget)
+
+    @abstractmethod
+    def could_use_max_tax(self, budget):
+        pass
+
+    @abstractmethod
+    def max_tax(self, budget):
+        pass
+
+    @abstractmethod
+    def min_tax(self, budget):
+        pass
+
 class ISS(object):
 
     def calculate(self, budget):
@@ -9,4 +34,38 @@ class ICMS(object):
 
     def calculate(self, budget):
 
+        return budget.value * 0.06
+
+class ICPP(Template_of_tax_conditional):
+
+    def could_use_max_tax(self, budget):
+        
+        return budget.value > 500
+            
+    def max_tax(self, budget):
+        return budget.value * 0.07
+
+    def min_tax(self, budget):
+        return budget.value * 0.05
+
+class IKCV(Template_of_tax_conditional):
+
+    def __has_item_greater_then_hundred_reais(self, budget):
+
+        for item in budget.get_items():
+            if item.value > 100:
+                return True
+        
+        return False
+
+    def could_use_max_tax(self, budget):
+        
+        return budget.value > 500 and self.__has_item_greater_then_hundred_reais(budget)
+            
+    def max_tax(self, budget):
+        
+        return budget.value * 0.1
+
+    def min_tax(self, budget):
+        
         return budget.value * 0.06
